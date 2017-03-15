5b744baaf8aaaaffe7200bd7ffc059eb53680550
Alex Seliverov <avseliverov@gmail.com>
2017-03-15 16:03:47 -0700

    Add Reverse_StringBuffer

---
+++ Reverse_StringBuffer.java
@@ -0,0 +1,12 @@
+package codingWithNumbers;
+
+public class Reverse_StringBuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string = "Reverse String";
+		String reverse = new StringBuffer(string).reverse().toString();
+		System.out.println(reverse);
+	}
+
+}
